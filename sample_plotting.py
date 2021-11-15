966af4e850745c96666cf7da901e570cb25f9602
saodem74 <saodem74@users.noreply.github.com>
2021-11-15 14:52:14 -0600

    add plotting utils

---
+++ sample_plotting.py
@@ -0,0 +1,33 @@
+import gym
+
+from stable_baselines3 import TD3
+from stable_baselines3.common.monitor import Monitor
+from tl.plot_utils import *
+from tl.new_pendulum_env import NewPendulumEnv
+
+
+
+# Create log dir
+log_dir = "/tmp/gym/"
+os.makedirs(log_dir, exist_ok=True)
+
+policy_name = 'MlpPolicy'
+step_number = 1000
+env_name = 'Pendulum-v1'
+
+source_env = gym.make(env_name)
+target_env = NewPendulumEnv(source_env)
+
+# Create and wrap the environment
+# env = gym.make('LunarLanderContinuous-v2')
+# Logs will be saved in log_dir/monitor.csv
+env = Monitor(target_env, log_dir)
+
+# Create the callback: check every 50 steps
+callback = SaveOnBestTrainingRewardCallback(check_freq=50, log_dir=log_dir)
+# Create RL model
+model = TD3('MlpPolicy', env, verbose=0)
+# Train the agent
+model.learn(total_timesteps=5000, callback=callback)
+
+plot_results(log_dir, moving_window=10)
---
+++ tl/plot_utils.py
@@ -0,0 +1,86 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from stable_baselines3.common.callbacks import BaseCallback
+
+from stable_baselines3.common.results_plotter import load_results, ts2xy
+
+def moving_average(values, window):
+    """
+    Smooth values by doing a moving average
+    :param values: (numpy array)
+    :param window: (int)
+    :return: (numpy array)
+    """
+    weights = np.repeat(1.0, window) / window
+    return np.convolve(values, weights, 'valid')
+
+
+def plot_results(log_folder, title='Learning Curve', moving_window=1):
+    """
+    plot the results
+
+    :param log_folder: (str) the save location of the results to plot
+    :param title: (str) the title of the task to plot
+    """
+    x, y = ts2xy(load_results(log_folder), 'timesteps')
+    print(x)
+    print(y)
+    y = moving_average(y, window=moving_window)
+    # Truncate x
+    x = x[len(x) - len(y):]
+
+    print(x)
+    print(y)
+
+    fig = plt.figure(title)
+    plt.plot(x, y)
+    plt.xlabel('Number of Timesteps')
+    plt.ylabel('Rewards')
+    plt.title(title + " Smoothed")
+    plt.show()
+
+
+class SaveOnBestTrainingRewardCallback(BaseCallback):
+    """
+    Callback for saving a model (the check is done every ``check_freq`` steps)
+    based on the training reward (in practice, we recommend using ``EvalCallback``).
+
+    :param check_freq: (int)
+    :param log_dir: (str) Path to the folder where the model will be saved.
+      It must contains the file created by the ``Monitor`` wrapper.
+    :param verbose: (int)
+    """
+    def __init__(self, check_freq: int, log_dir: str, verbose=1):
+        super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.log_dir = log_dir
+        self.save_path = os.path.join(log_dir, 'best_model')
+        self.best_mean_reward = -np.inf
+
+    def _init_callback(self) -> None:
+        # Create folder if needed
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self) -> bool:
+        if self.n_calls % self.check_freq == 0:
+
+          # Retrieve training reward
+          x, y = ts2xy(load_results(self.log_dir), 'timesteps')
+          if len(x) > 0:
+              # Mean training reward over the last 100 episodes
+              mean_reward = np.mean(y[-100:])
+              if self.verbose > 0:
+                print(f"Num timesteps: {self.num_timesteps}")
+                print(f"Best mean reward: {self.best_mean_reward:.2f} - Last mean reward per episode: {mean_reward:.2f}")
+
+              # New best model, you could save the agent here
+              if mean_reward > self.best_mean_reward:
+                  self.best_mean_reward = mean_reward
+                  # Example for saving best model
+                  if self.verbose > 0:
+                    print(f"Saving new best model to {self.save_path}.zip")
+                  self.model.save(self.save_path)
+
+        return True
