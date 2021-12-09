f8c0f51292f55b3e5d217d9cca17dd8b84cdb08d
nab170130 <nab170130@utdallas.edu>
2021-12-09 00:55:11 -0600

    Fix Plotting
    Changed moving average computation to account for edge averages; fixed issue where episode counts count differ across runs of the same strategy

--- tl/utils/plot_utils.py
+++ tl/utils/plot_utils.py
@@ -5,26 +5,30 @@ import matplotlib.pyplot as plt
 from stable_baselines3.common.callbacks import BaseCallback
 
 from stable_baselines3.common.results_plotter import load_results, ts2xy
-
-def moving_average(values, window):
+   
+def moving_average(values, window_size):
+    
     """
     Smooth values by doing a moving average
     :param values: (numpy array)
     :param window: (int)
     :return: (numpy array)
     """
-    if window == -1: # average from begining to current position
-        moving_averages = []
-        i = 0
-        s = 0
-        while i < len(values):
-            s += values[i]
-            moving_averages.append(s / (i + 1))
-            i += 1
-        return moving_averages
-
-    weights = np.repeat(1.0, window) / window
-    return np.convolve(values, weights, 'valid')
+    if window_size % 2 == 0:
+        raise ValueError("Please pick an odd window size.")
+    
+    # Create a filter that serves to average the window_size elements about each element.
+    averaging_filter = np.repeat(1.0, window_size) / window_size
+    
+    # Do a convolution, ensuring only to keep the center elements such that |conv| = len(values)
+    moving_averages = np.convolve(averaging_filter, values, mode='same')
+    
+    # Moving averages need to be fixed at the ends as they were not averaged correctly.
+    for i in range(window_size // 2 + 1):
+        moving_averages[i] = moving_averages[i] * window_size / (window_size // 2 + 1 + i)
+        moving_averages[len(moving_averages) - 1 - i] = moving_averages[len(moving_averages) - 1 - i] * window_size / (window_size // 2 + 1 + i)
+        
+    return moving_averages
 
 
 def plot_results(log_folder, title='Learning Curve', moving_window=-1):
@@ -123,14 +127,24 @@ def loading_all_exp_result_from_directory(dir, running_time_num=1):
     for i in range(running_time_num):
         new_dir = dir + str(i) + "/"
         xx, yy = ts2xy(load_results(new_dir), 'episodes')
-        x = xx
+        x.append(xx) # Keep track of all x's; turns out that not all runs have same episode count.
         y.append(yy)
 
-    return x, y
+    # Determine run with least episodes. Adjust ALL runs to have only as many as the one with the least episodes.
+    episode_count = len(x[0])    
+    for i in range(running_time_num):
+        if len(x[i]) < episode_count:
+            episode_count = len(x[i])
+    
+    for i in range(running_time_num):
+        x[i] = (x[i])[:episode_count]
+        y[i] = (y[i])[:episode_count]
+
+    return x[0], y
 
 
 def extract_xy_for_plotting(dir, running_time_num, moving_window):
-    x, y = loading_all_exp_result_from_directory(dir, running_time_num)
+    x_plot, y = loading_all_exp_result_from_directory(dir, running_time_num)
 
     y_moving_averages = []
     for episode_reward_list in y:
@@ -142,7 +156,6 @@ def extract_xy_for_plotting(dir, running_time_num, moving_window):
     upper_list = [(x + y) for (x, y) in zip(y_ave, y_std)]
 
     y_plot = y_ave
-    x_plot = x[len(x) - len(y_plot):]
 
     return x_plot, y_plot, lower_list, upper_list
 
@@ -169,16 +182,16 @@ def plot_multiple_results_with_multiple_runing_time(log_dir_w_TL,
     x_wo_TL, y_wo_TL, wo_TL_lower_list, wo_TL_upper_list = extract_xy_for_plotting(log_dir_wo_TL, running_time_num,
                                                                                moving_window)
 
-    plt.figure(title)
+    plt.figure(title, figdim=(16,9))
 
     plt.plot(x_wo_TL, y_wo_TL, marker='x', markersize=8, linestyle='-', color='b', label='Without TL', linewidth=3)
-    plt.fill_between(x_wo_TL, wo_TL_lower_list, wo_TL_upper_list, color='lightblue', alpha=0.2)
+    plt.fill_between(x_wo_TL, wo_TL_lower_list, wo_TL_upper_list, color='lightblue', alpha=0.4)
 
     plt.plot(x_w_TL, y_w_TL, marker='o', markersize=8, linestyle='-', color='g', label='With TL',  linewidth=3)
-    plt.fill_between(x_w_TL, w_TL_lower_list, w_TL_upper_list, color='lightgreen', alpha=0.2)
+    plt.fill_between(x_w_TL, w_TL_lower_list, w_TL_upper_list, color='lightgreen', alpha=0.4)
 
     plt.plot(x_w_TL_rs, y_w_TL_rs, marker='s', markersize=8, linestyle='-', color='r', label='With TL rs', linewidth=3)
-    plt.fill_between(x_w_TL_rs, w_TL_rs_lower_list, w_TL_rs_upper_list, color='lightcoral', alpha=0.2)
+    plt.fill_between(x_w_TL_rs, w_TL_rs_lower_list, w_TL_rs_upper_list, color='lightcoral', alpha=0.4)
 
     plt.legend(loc='upper left', title='Approaches', fontsize=14)
     plt.xlabel('Number of Episodes')
